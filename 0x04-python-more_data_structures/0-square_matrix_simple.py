e35ef991c90e0ff2bbd17d4494ceec090617b7b8
hileamlak <hilea.buisness@gmail.com>
2020-12-17 15:10:14 +0000

    Change funtion name

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,6 @@
 # 0-square_matrix_simple.py
 
 
-def square_matrix(matrix=[]):
+def square_matrix_simple(matrix=[]):
     """Compute the square value of all integers of a matrix."""
     return ([[col * col for col in row] for row in matrix])
