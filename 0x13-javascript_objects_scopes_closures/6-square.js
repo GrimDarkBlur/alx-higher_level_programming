483d8f66d001276595eb762c52fa9ec289929f19
hileamlak <hilea.buisness@gmail.com>
2021-03-23 20:33:09 +0000

    task 0x13

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,52 @@
+#!/usr/bin/node
+/*
+   Defines a square calss that inherits from the
+rectangle class
+ */
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('x');
+      }
+      console.log('');
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write(c);
+      }
+      console.log('');
+    }
+  }
+};
