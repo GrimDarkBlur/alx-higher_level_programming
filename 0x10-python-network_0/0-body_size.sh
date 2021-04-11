91631f51dec520dcd0d79fe13e0a2412a02e1555
hileamlak <hilea.buisness@gmail.com>
2021-04-11 18:51:32 +0000

    Improve stle'

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,5 +2,5 @@
 # prints the content length of a header
 # of a get requests using curl
 
-curl -Il $1 2>&1|grep content-length -i| cut -d' ' -f2
+curl -Il "$1" 2>&1|grep content-length -i| cut -d' ' -f2
 
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
-#!/usr/env/bin bash
+#!/usr/bin/env bash
 # Bash scripts that sends a POST request to a given URL.
 curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
