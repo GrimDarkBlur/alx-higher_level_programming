1e8a87dbe41281a39fc56c71cbe86ce24f520519
hileamlak <hilea.buisness@gmail.com>
2021-04-24 07:49:11 +0000

    Make minor changes

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,5 +2,5 @@
 # prints the content length of a header
 # of a get requests using curl
 
-curl -Il "$1" 2>&1|grep content-length -i| cut -d' ' -f2
+curl -Is "$1" |grep "Content-Length:"| cut -d' ' -f2
 
