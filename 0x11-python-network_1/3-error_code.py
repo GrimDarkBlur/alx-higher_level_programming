2d90132e57a94eba14a0ed88b125c2e2e9c53ed2
hileamlak <hilea.buisness@gmail.com>
2021-04-24 08:45:07 +0000

    Adhere to linter rules

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -13,7 +13,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
     try:
         with urllib.request.urlopen(url) as response:
-            #print(help(response.info()))
             print(response.read().decode('utf-8'))
     except urllib.error.URLError as e:
         print("Error code: {}".format(e.code))
